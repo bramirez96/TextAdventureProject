7af2772973dc87af56f534b9b85b8b2ea3b29efb
Brandon Ramirez <bran.ramirez.don@gmail.com>
2020-07-19 23:48:48 -0700

    Added comments to world reading function

--- textadventure/world.py
+++ textadventure/world.py
@@ -4,16 +4,24 @@ starting_position = (0, 0)
 def load_tiles():
   with open("data/map.txt", "r") as f:
     rows = f.readlines()
+  
   x_max = len(rows[0].split())
+
+  # loop over all elements read in from map file
   for y in range(len(rows)):
     cols = rows[y].split()
     for x in range(x_max):
       tile_name = cols[x].replace("\n", " ")
+
+      # change this to change starting tile
       if tile_name == "AptBed":
         global starting_position
         starting_position = (x, y)
+
+      # checks current coordinates
       _world[(x, y)] = None if tile_name == "." \
         else getattr(__import__("tiles"), tile_name)(x, y)
       
 def tile_exists(x, y):
+  # returns none or returns a room instance
   return _world.get((x, y))
